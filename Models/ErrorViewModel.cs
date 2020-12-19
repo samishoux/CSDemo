f30d450d6adc26c694c2c49bd08966490016df4b
samishoux <samuel.chabot@live.ca>
2020-12-18 23:42:00 -0500

    first commit

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Codeboxx_Stack_Demo.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
